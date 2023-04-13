e865c0175ca3a34ef7f26e7e6e587cc4bbc8f9c1
Anton Vedischev <vedishchevanton@gmail.com>
2023-04-14 00:22:36 +0300

    изменения в структуре

--- src/utils.py
+++ src/utils.py
@@ -1,121 +1,56 @@
-"""Основные методы для реализации программы"""
 from src.headhunter_api import HeadHunterAPI
 from src.superjob_api import SuperJobAPI
 from src.jsonsaver_class import JSONSaver
-from src.user_operations import UserOperations
-
-API_KEY = 'v3.r.137482765.bec7916c78cac4d8735ecab826d1fa6d374196d3.b0819ec4fbe617fcea37ae324dd6804053e04b87'
 
 
-def search_query():
+def get_user_query():
     """
     Метод получения поискового запроса
     """
-    return input("Введите поисковый запрос(например Python): ")
+    return input("Введите поисковый запрос(например Python developer): ")
 
 
-def resource_selection():
+def get_user_resource():
     """
     Метод выбора ресурса
     """
-    print(
-        "Выберите ресурс для поиска и обработки вакансии: \n"
-        "Если вы хотите выбрать HeadHunter.ru, нажмите '1'\n"
-        "Если вы хотите выбрать SuperJob.ru, нажмите '2'\n"
-        "Если вы хотите использовать оба ресурса, нажмите '3'"
-    )
-    while True:
-        resource = input()
-        if resource == '1':
-            print('Вы выбрали HeadHunter.ru')
-            return 1
-        elif resource == '2':
-            print('Вы выбрали Superjob.ru')
-            return 2
-        elif resource == '3':
-            print('Вы выбрали оба ресурса ')
-            return 3
-        else:
-            print("Некорректный ввод данных! Следуйте инструкциям, которые указаны выше.")
+    user_input = input()
+    if user_input == '1':
+        print('Вы выбрали HeadHunter.ru')
+        hh = HeadHunterAPI()
+        return hh.get_vacancies(get_user_query())
+    elif user_input == '2':
+        print('Вы выбрали Superjob.ru')
+        sj = SuperJobAPI()
+        return sj.get_vacancies(get_user_query())
+    else:
+        print('Некорректный ввод')
 
 
-def get_user_request():
+def data_filter(vacancies):
     """
-    Метод поиска запрашиваемых данных на выбранном ресурсе
+    Метод работы с полученными вакансиями
     """
     while True:
-        if resource_selection() == 1:
-            hh = HeadHunterAPI()
-            return hh.get_vacancies(search_query())
-        elif resource_selection() == 2:
-            sj = SuperJobAPI()
-            return sj.get_vacancies(search_query())
-        elif resource_selection() == 3:
-            hh = HeadHunterAPI()
-            hh_vacancies = hh.get_vacancies(search_query())
-            sj = SuperJobAPI()
-            sj_vacancies = sj.get_vacancies(search_query())
-            return hh_vacancies + sj_vacancies
-
-
-def work_with_file(vacancies):
-    """
-    Метод работы с данными от лица пользователя
-    """
-    while True:
-        print(
-            "Если хотите сохранить файл и выйти, нажмите '1'\n"
-            "Если хотите произвести сортировку вакансий по ключевому слову, нажмите '2'\n"
-            "Если хотите произвести сортировку вакансий по заработной плате, нажмите '3'\n"
-            "Если хотите произвести сортировку по выбранным вами вакансиям, нажмите '4'"
-        )
-
-        user = input()
-        if user == '1':
-            return vacancies
-        elif user == '2':
+        print("Вы зашли в меню работы с данными:\n"
+              "Если вы хотите сохранить полученные данные в файл и выйти, нажмите '1'\n"
+              "Если вы хотите произвести фильтрацию вакансий по ключевому слову, нажмите '2'"
+              )
+        user_input = input()
+        if user_input == '1':
+            json_saver = JSONSaver(vacancies)
+            return json_saver.dump_to_file()
+        elif user_input == '2':
             filter_words = input("Введите ключевое слово для фильтрации вакансий: ")
-            vacancies = UserOperations(vacancies, filter_words,
-                                       top_count=0).filter_vacancies()
-            if not vacancies:
-                print("Нет вакансий, соответствующих заданным критериям.")
-                return
-            print("Операция выполнена!")
-        elif user == '3':
-            vacancies = UserOperations(vacancies).sorting()  # Сортировка полученных данных
-            print("Операция выполнена!")
-        elif user == '4':
-            while True:
-                try:
-                    top_count = float(input("Введите количество вакансий: "))
-                    vacancies = UserOperations(vacancies, top_count).get_top()  # Отсортировка топ N-количества вакансий
-                    print("Операция выполнена!")
-                    break
-                except ValueError:
-                    print("Вы ввели не число. Пожалуйста, попробуйте снова.")
-        else:
-            print("Неверное значение! Введите цифру от 1 - 4!")
-
-
-def check_vacancies_data():
-    """
-    Метод проверки найденных по запросу вакансий
-    """
-    while True:
-        vacancies_data = get_user_request()
-        for vacancies in vacancies_data:
-            if vacancies:
-                return vacancies_data
-            print("Ваш запрос не найден!")
+            sorted_vacancies = sorted(vacancies, key=lambda x: x[filter_words])
+            json_saver = JSONSaver(sorted_vacancies)
+            return json_saver.dump_to_file()
 
 
 def main():
-    print('Вас приветствует программа по сбору и обработке данных с ресурсов HeadHunter.ru и SuperJob.ru.')
-    print()
-    vacancies_data = check_vacancies_data()
-    json_saver = JSONSaver(vacancies_data)
-    json_saver.add_vacancies()
-    vacancies = json_saver.data_file()
-    user_vacancies = work_with_file(vacancies)
-    JSONSaver(user_vacancies).get_user_file()
-    print("Данные сохранены и записаны в файл 'users_sort.json'")
+    print("Вас приветствует программа по сбору и обработке данных с ресурсов HeadHunter.ru и SuperJob.ru!\n"
+          "Если вы хотите выбрать HeadHunter.ru, нажмите '1'\n"
+          "Если вы хотите выбрать SuperJob.ru, нажмите '2'"
+          )
+    user_input = get_user_resource()
+    data_filter(user_input)
