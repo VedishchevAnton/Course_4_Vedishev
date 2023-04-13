9b45cb357a03afabbb70c5a719eb4774ba55a373
Anton Vedischev <vedishchevanton@gmail.com>
2023-04-14 01:34:52 +0300

    изменени в методах

--- src/jsonsaver_class.py
+++ src/jsonsaver_class.py
@@ -5,13 +5,14 @@ from src.headhunter_api import HeadHunterAPI
 
 
 class JSONSaver(Saver):
+    def __init__(self, filename):
+        self.filename = filename
+        self.vacancies = []
 
-    def __init__(self, data: list):
-        super().__init__(data)
-
-    def dump_to_file(self):
+    def dump_to_file(self, **kwargs):
         """
         Метод записи файла с вакансиями
+        :param **kwargs:
         """
         with open("data.json", 'w', encoding='utf-8') as outfile:
-            json.dump(self.data, outfile, indent=1, ensure_ascii=False)
+            json.dump(self.filename, outfile, indent=1, ensure_ascii=False)
