6a6f423ff93cc9521c79502abc2c9446398025ce
Anton Vedischev <vedishchevanton@gmail.com>
2023-04-15 13:19:56 +0300

    изменение в get_vacancies и  добавление статического метода

--- src/headhunter_api.py
+++ src/headhunter_api.py
@@ -1,34 +1,49 @@
 """Реализация класса HeadHunterAPI, наследующегося от абстрактного класса, для работы с ресурсом HeadHunter.ru"""
-import requests
 from src.engine_class import Engine
+from src.vacancy_class import Vacancy
+import requests
 
 
 class HeadHunterAPI(Engine):
     def __init__(self):
-        self.vacancies = []
+        self.url_hh = "https://api.hh.ru/vacancies"
 
-    def get_vacancies(self, search_query: str):
+    def get_vacancies(self, search_query):
         """
-        Метод настройки запроса
-        :return: json object
+        Метод для получения вакансий с помощью HeadHunterApi.
+
+        Search_query Ключевое слово для поиска вакансии.
+        Return: Список объектов класса Vacancy.
         """
-        url = "https://api.hh.ru/vacancies"
-        params = {
-            "text": search_query,
-            "page": 10,
-            "per_page": 100,
-            "area": 113,  # Россия
-            "only_with_salary": True
-        }
-        response = requests.get(url=url, params=params)
+        params = {'text': search_query,
+                  'per_page': 100,
+                  'area': 113
+                  }
+        response = requests.get(self.url_hh, params=params)
         if response.status_code == 200:
-            vacancies = response.json()["items"]
-            for vacancy in vacancies:
-                self.vacancies.append({'name': vacancy['name'],
-                                       'url': vacancy['url'],
-                                       'description': vacancy['snippet']['requirement'],
-                                       'payment': vacancy['salary']
-                                       })
-            return self.vacancies
+            data = response.json()
+            vacancies_data = data['items']
+            vacancies = []
+            for vacancy in vacancies_data:
+                title = vacancy['name']
+                salary = HeadHunterAPI.get_salary(vacancy['salary'])
+                description = vacancy['snippet']['requirement']
+                url = vacancy['alternate_url']
+                vacancy = Vacancy(title, salary, description, url)
+                vacancies.append(vacancy)
+            return vacancies
         else:
             return f'Error: {response.status_code}'
+
+    @staticmethod
+    def get_salary(salary_data, **kwargs):
+        if salary_data is None:
+            salary = {'from': 0, 'currency': 'RUR'}
+        elif 'to' not in salary_data or salary_data['to'] is None:
+            salary = {'from': salary_data.get('from', 0), 'currency': salary_data.get('currency', 'RUR')}
+        elif 'from' not in salary_data or salary_data['from'] is None:
+            salary = {'from': salary_data['to'], 'currency': salary_data.get('currency', 'RUR')}
+        else:
+            salary = {'from': salary_data['from'], 'to': salary_data['to'],
+                      'currency': salary_data.get('currency', 'RUR')}
+        return salary
