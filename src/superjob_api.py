63527492f4fc431e4a526356451f8c89e7a7b54c
Anton Vedischev <vedishchevanton@gmail.com>
2023-04-15 13:23:00 +0300

    добавление статического метода

--- src/superjob_api.py
+++ src/superjob_api.py
@@ -2,33 +2,51 @@
 import requests
 import os
 from src.engine_class import Engine
+from src.vacancy_class import Vacancy
 
 
 class SuperJobAPI(Engine):
 
     def __init__(self):
-        self.vacancies = []
         self.api_key: str = os.getenv('API_KEY')
         self.headers = {'X-Api-App-Id': self.api_key}
         self.url = "https://api.superjob.ru/2.0/vacancies/"
 
     def get_vacancies(self, search_query: str):
+        """
+            Метод для получения вакансий с помощью SuperJobApi.
+
+            Search_query Ключевое слово для поиска вакансии.
+            Return: Список объектов класса Vacancy.
+            """
         params = {'keyword': search_query,
                   'count': 100,
-                  'page': 1
                   }
 
         response = requests.get(self.url, headers=self.headers, params=params)
         if response.status_code == 200:
             data = response.json()
             vacancies_data = data['objects']
+            vacancies = []
             for vacancy in vacancies_data:
-                vacancy_data = {'name': vacancy['profession'],
-                                'url': vacancy['link'],
-                                'description': vacancy['candidat'],
-                                'payment': vacancy['payment_from']
-                                }
-                self.vacancies.append(vacancy_data)
-            return self.vacancies
+                title = vacancy['profession']
+                salary = SuperJobAPI.get_salary(vacancy)
+                description = vacancy['candidat']
+                url = vacancy['link']
+                vacancy = Vacancy(title, salary, description, url)
+                vacancies.append(vacancy)
+            return vacancies
         else:
             return f'Error: {response.status_code}'
+
+    @staticmethod
+    def get_salary(vacancy, **kwargs):
+        if vacancy.get('payment_to') == 0:
+            salary = {'from': vacancy['payment_from'], 'currency': vacancy['currency']}
+        elif vacancy.get('payment_from') == 0:
+            salary = {'from': vacancy['payment_to'], 'currency': vacancy['currency']}
+        else:
+            salary = {'from': vacancy.get('payment_from', 0), 'to': vacancy.get('payment_to', 0),
+                      'currency': vacancy.get('currency', 'rub')}
+        return salary
+
